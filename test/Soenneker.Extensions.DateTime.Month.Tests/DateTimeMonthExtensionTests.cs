14fa450bd1ee0a64f7e4793d2b50599b067cee30
Jake Soenneker <jake@soenneker.com>
2026-04-22 18:28:05 -0500

    Fixes

--- test/Soenneker.Extensions.DateTime.Month.Tests/DateTimeMonthExtensionTests.cs
+++ test/Soenneker.Extensions.DateTime.Month.Tests/DateTimeMonthExtensionTests.cs
@@ -59,9 +59,9 @@ public class DateTimeMonthExtensionTests : UnitTest
         converted.Second.Should().Be(59);
     }
 
-    [Theory]
-    [InlineData("2023-08-15", "2023-08-01")]
-    [InlineData("2023-02-28", "2023-02-01")]
+    [Test]
+    [Arguments("2023-08-15", "2023-08-01")]
+    [Arguments("2023-02-28", "2023-02-01")]
     public void ToStartOfMonth_ReturnsFirstDayOfMonthAtMidnight(string testDate, string expectedDate)
     {
         // Arrange
@@ -74,9 +74,9 @@ public class DateTimeMonthExtensionTests : UnitTest
         result.Should().Be(System.DateTime.Parse(expectedDate));
     }
 
-    [Theory]
-    [InlineData("2023-03-01", "2023-03-31 23:59:59.9999999")]
-    [InlineData("2023-02-15", "2023-02-28 23:59:59.9999999")] // Leap year not accounted
+    [Test]
+    [Arguments("2023-03-01", "2023-03-31 23:59:59.9999999")]
+    [Arguments("2023-02-15", "2023-02-28 23:59:59.9999999")] // Leap year not accounted
     public void ToEndOfMonth_ReturnsLastMomentOfMonth(string testDate, string expectedDate)
     {
         // Arrange
@@ -89,9 +89,9 @@ public class DateTimeMonthExtensionTests : UnitTest
         result.Should().BeCloseTo(System.DateTime.Parse(expectedDate), TimeSpan.FromMilliseconds(1));
     }
 
-    [Theory]
-    [InlineData("2023-03-15", "2023-04-01")]
-    [InlineData("2023-12-31", "2024-01-01")]
+    [Test]
+    [Arguments("2023-03-15", "2023-04-01")]
+    [Arguments("2023-12-31", "2024-01-01")]
     public void ToStartOfNextMonth_ReturnsFirstDayOfNextMonthAtMidnight(string testDate, string expectedDate)
     {
         // Arrange
@@ -104,9 +104,9 @@ public class DateTimeMonthExtensionTests : UnitTest
         result.Should().Be(System.DateTime.Parse(expectedDate));
     }
 
-    [Theory]
-    [InlineData("2023-03-15", "2023-02-01")]
-    [InlineData("2023-01-01", "2022-12-01")]
+    [Test]
+    [Arguments("2023-03-15", "2023-02-01")]
+    [Arguments("2023-01-01", "2022-12-01")]
     public void ToStartOfPreviousMonth_ReturnsFirstDayOfPreviousMonthAtMidnight(string testDate, string expectedDate)
     {
         // Arrange
